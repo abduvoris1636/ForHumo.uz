285c4d16f8b7b1b2d097dc163eec5a4ec740a7e5
abduvoris1636 <abduvoriskakhramonov@gmail.com>
2025-12-17 21:03:41 +0500

    Update Log.js

--- models/Log.js
+++ models/Log.js
@@ -1,1 +1,24 @@
+import mongoose from 'mongoose';
 
+const LogSchema = new mongoose.Schema({
+  action: {
+    type: String,
+    required: true,
+    enum: ['team_registered', 'team_approved', 'team_deleted', 'admin_login', 'error']
+  },
+  details: {
+    type: mongoose.Schema.Types.Mixed
+  },
+  ipAddress: {
+    type: String
+  },
+  userAgent: {
+    type: String
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.models.Log || mongoose.model('Log', LogSchema);
