59904b15b94298832286c9adbddc911e2de6ca88
abduvoris1636 <abduvoriskakhramonov@gmail.com>
2025-12-17 21:03:32 +0500

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,1 +1,23 @@
+import mongoose from 'mongoose';
 
+const AdminSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  },
+  lastLogin: {
+    type: Date
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.models.Admin || mongoose.model('Admin', AdminSchema);
