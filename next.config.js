12b1cbc71b1d5f1cfbc0f0df1effba15e9f476bb
abduvoris1636 <abduvoriskakhramonov@gmail.com>
2025-12-18 10:13:53 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,23 @@ const nextConfig = {
         hostname: 'res.cloudinary.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '**.cloudinary.com',
+        pathname: '/**',
+      },
     ],
+    domains: ['res.cloudinary.com'],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose', 'cloudinary'],
+  },
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    });
+    return config;
   },
 }
 
